989f8aadb0fbc00506bafd0ea3265c8e28da2f40
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-05 10:24:07 +0200

    Update 'Task' model

--- todo/models.py
+++ todo/models.py
@@ -13,9 +13,9 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
-    created = models.DateField(auto_now_add=True)
-    deadline = models.DateField(blank=True)
-    is_done = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tags")
 
     class Meta:
