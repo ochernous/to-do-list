1dc330dcd8ccf20b2bd8a096105d3d3ace5295b4
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-05 10:42:24 +0200

    Update 'Task' model - change deadline field

--- todo/models.py
+++ todo/models.py
@@ -14,7 +14,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateField(null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tags")
 
