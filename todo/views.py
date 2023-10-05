b39c32512c127c03d930d4ac97b167fa988af498
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-05 14:23:31 +0200

    Update 'views.py' - add create, update and delete views

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,31 @@
 # from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Tag, Task
 
 
-class TasksListView(generic.ListView):
+class TaskListView(generic.ListView):
     model = Task
     template_name = "todo/index.html"
 
 
-class TagsListView(generic.ListView):
+class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
