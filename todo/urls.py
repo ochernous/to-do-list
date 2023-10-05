e034d04566ebcf4f72be033aed7cd0508b8ab869
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-05 13:01:52 +0200

    Add 'tags' path

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from todo.views import TasksListView
+from todo.views import TasksListView, TagsListView
 
 
 urlpatterns = [
     path("", TasksListView.as_view(), name="task-list"),
+    path("tags/", TagsListView.as_view(), name="tags"),
 ]
 
 app_name = "todo"
