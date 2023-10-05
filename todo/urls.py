e6d148266c453dcceff977cdd6a3efab70881011
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-05 14:22:51 +0200

    Update 'urls.py'

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
-from todo.views import TasksListView, TagsListView
+from todo.views import (
+    TaskListView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 
 urlpatterns = [
-    path("", TasksListView.as_view(), name="task-list"),
-    path("tags/", TagsListView.as_view(), name="tags"),
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo"
